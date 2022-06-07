21e456f0cefe39664b3226bde583f5e39c8dcedb
LEON <dlwotjr2@gmail.com>
2022-06-07 18:07:11 +0900

    [Refactor]: 이벤트 위임 방식 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 import "./App.css";
-import { useState } from "react";
+import { MouseEvent, useState } from "react";
 import Canvas from "./components/Canvas";
 import Edits from "./components/Edits";
 
 function App() {
   const [src, setSrc] = useState("/src/assets/Pearl/기쁨00.jpg");
-  function create_handler(people: string[], emotions: string[]) {
-    // TODO: 랜덤함수
-    const newSrc = "/src/assets/Chim/분노00.PNG";
-    setSrc(newSrc);
+
+  function clickHandler({ target }: MouseEvent) {
+    if (!(target instanceof Element)) return;
+    if (target.className === "filter-list__emotion") {
+      console.log(target.id);
+    }
+    if (target.className === "filter-list__people") {
+      console.log(target.id);
+    }
+    if (target.className === "save-button") {
+      const canvas = document.querySelector("canvas");
+      const image = canvas?.toDataURL("image/png");
+      if (!image) return;
+      const link = document.createElement("a");
+      link.href = image;
+      link.download = "침투부-랜덤짤";
+      link.click();
+    }
+    if (target.className === "create-button") {
+      const newSrc = "/src/assets/Chim/분노00.PNG";
+      setSrc(newSrc);
+    }
   }
 
   return (
-    <div className="App">
+    <div onClick={clickHandler} className="App">
       <Canvas src={src} key={src} />
-      <Edits create_handler={create_handler} />
+      <Edits />
     </div>
   );
 }
--- src/components/Edits.tsx
+++ src/components/Edits.tsx
@@ -1,23 +1,30 @@
-type EditsProps = {
-  create_handler: (people: string[], emotions: string[]) => void;
-};
+import "./Edits.css";
 
-export default function Edits({ create_handler }: EditsProps) {
-  function save_handler() {
-    const canvas = document.querySelector("canvas");
-    const image = canvas?.toDataURL("image/png");
-    if (!image) return;
-    const link = document.createElement("a");
-    link.href = image;
-    link.download = "침투부-랜덤짤";
-    link.click();
-  }
-
+export default function Edits() {
   return (
-    <div>
-      <section>필터</section>
-      <button onClick={() => create_handler(["A"], ["A"])}>생성</button>
-      <button onClick={save_handler}>저장</button>
+    <div className="edits">
+      <section>
+        <ul className="filter-list">
+          <li className="filter-list__people" id="chim">
+            침착맨
+          </li>
+          <li className="filter-list__people">주호민</li>
+          <li className="filter-list__people">김풍</li>
+          <li className="filter-list__people">궤도</li>
+          <li className="filter-list__people">통천</li>
+        </ul>
+        <ul className="filter-list">
+          <li className="filter-list__emotion">일갈</li>
+          <li className="filter-list__emotion">비난</li>
+          <li className="filter-list__emotion">칭찬</li>
+          <li className="filter-list__emotion">눈물</li>
+          <li className="filter-list__emotion">기쁨</li>
+        </ul>
+      </section>
+      <section className="button-section">
+        <button className="create-button">생성</button>
+        <button className="save-button">저장</button>
+      </section>
     </div>
   );
 }
