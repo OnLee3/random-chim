5d5248e48849f13e1ede98608e790f39ae6299bd
LEON <dlwotjr2@gmail.com>
2022-06-07 15:42:23 +0900

    [Feature]: App 틀 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
+import { useState } from "react";
 import Canvas from "./components/Canvas";
+import Edits from "./components/Edits";
 
 function App() {
+  const [src, setSrc] = useState("/src/assets/Pearl/기쁨00.jpg");
+  function create_handler(people: string[], emotions: string[]) {
+    // TODO: 랜덤함수
+    const newSrc = "/src/assets/Chim/분노00.PNG";
+    setSrc(newSrc);
+  }
+
   return (
     <div className="App">
-      <Canvas />
-      <section>EDIT</section>
+      <Canvas src={src} key={src} />
+      <Edits create_handler={create_handler} />
     </div>
   );
 }
