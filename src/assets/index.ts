9ddf9d18c5d0ff85e2349c0256853887f35ae945
LEON <dlwotjr2@gmail.com>
2022-06-10 15:57:01 +0900

    [Assets]: 이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,13 @@
 import j_joy00 from "./img/Pearl/joy00.jpg";
 import j_joy01 from "./img/Pearl/joy01.gif";
+import j_angry00 from "./img/Pearl/angry00.jpg";
 
 import c_angry00 from "./img/Chim/angry00.png";
 import c_angry01 from "./img/Chim/angry01.jpg";
 import c_joy00 from "./img/Chim/joy00.jpg";
-4;
 
 import o_angry00 from "./img/Others/angry00_gue.png";
+import o_joy00 from "./img/Others/joy00_gue.jpg";
 
 const db = {
   침착맨: {
@@ -15,8 +16,10 @@ const db = {
   },
   주호민: {
     joy: [j_joy00, j_joy01],
+    angry: [j_angry00],
   },
   주변인물: {
+    joy: [o_joy00],
     angry: [o_angry00],
   },
 };
