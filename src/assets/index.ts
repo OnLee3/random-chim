6fff1e81a96cfaad0ba57dd59942064a315761da
LEON <dlwotjr2@gmail.com>
2022-06-22 16:16:58 +0900

    [Assets]: 이미지 업로드

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,22 +1,82 @@
-import j_joy00 from "./img/Pearl/joy00.jpg";
-import j_joy01 from "./img/Pearl/joy01.gif";
-import j_angry00 from "./img/Pearl/angry00.jpg";
+import j_joy00 from "./img/Pearl/joy00.jpeg";
+import j_joy01 from "./img/Pearl/joy01.jpg";
+import j_joy02 from "./img/Pearl/joy02.jpg";
+import j_angry00 from "./img/Pearl/Pearl-angry00.jpg";
+import j_angry01 from "./img/Pearl/Pearl-angry01.jpg";
+import j_angry02 from "./img/Pearl/Pearl-angry02.jpg";
+import j_angry03 from "./img/Pearl/Pearl-angry03.jpg";
+import j_angry04 from "./img/Pearl/Pearl-angry04.jpg";
 
-import c_angry00 from "./img/Chim/angry00.png";
-import c_angry01 from "./img/Chim/angry01.jpg";
-import c_joy00 from "./img/Chim/joy00.jpg";
+import c_angry00 from "./img/Chim/Chim-angry00.jpg";
+import c_angry01 from "./img/Chim/Chim-angry01.jpeg";
+import c_angry02 from "./img/Chim/Chim-angry02.jpg";
+import c_angry03 from "./img/Chim/Chim-angry03.jpg";
+import c_angry04 from "./img/Chim/Chim-angry04.jpg";
+import c_angry05 from "./img/Chim/Chim-angry05.jpg";
+import c_angry06 from "./img/Chim/Chim-angry06.jpg";
+import c_angry07 from "./img/Chim/Chim-angry07.jpg";
+import c_angry08 from "./img/Chim/Chim-angry08.jpg";
+import c_angry09 from "./img/Chim/Chim-angry09.jpg";
+import c_angry10 from "./img/Chim/Chim-angry10.jpg";
+import c_angry11 from "./img/Chim/Chim-angry11.jpg";
+import c_angry12 from "./img/Chim/Chim-angry12.jpeg";
+import c_angry13 from "./img/Chim/Chim-angry13.jpeg";
+import c_angry14 from "./img/Chim/Chim-angry14.jpg";
+import c_angry15 from "./img/Chim/Chim-angry15.jpg";
+import c_joy00 from "./img/Chim/Chim-joy00.jpg";
+import c_joy01 from "./img/Chim/Chim-joy01.jpg";
+import c_joy02 from "./img/Chim/Chim-joy02.jpg";
+import c_joy03 from "./img/Chim/Chim-joy03.jpg";
+import c_joy04 from "./img/Chim/Chim-joy04.jpeg";
+import c_joy05 from "./img/Chim/Chim-joy05.jpg";
+import c_joy06 from "./img/Chim/Chim-joy06.jpg";
+import c_joy07 from "./img/Chim/Chim-joy07.jpg";
+import c_joy08 from "./img/Chim/Chim-joy08.jpg";
+import c_joy09 from "./img/Chim/Chim-joy09.jpg";
+import c_joy10 from "./img/Chim/Chim-joy10.jpg";
+import c_joy11 from "./img/Chim/Chim-joy11.jpg";
 
 import o_angry00 from "./img/Others/angry00_gue.png";
 import o_joy00 from "./img/Others/joy00_gue.jpg";
 
 const db = {
   침착맨: {
-    joy: [c_joy00],
-    angry: [c_angry00, c_angry01],
+    joy: [
+      c_joy00,
+      c_joy01,
+      c_joy02,
+      c_joy03,
+      c_joy04,
+      c_joy05,
+      c_joy06,
+      c_joy07,
+      c_joy08,
+      c_joy09,
+      c_joy10,
+      c_joy11,
+    ],
+    angry: [
+      c_angry00,
+      c_angry01,
+      c_angry02,
+      c_angry03,
+      c_angry04,
+      c_angry05,
+      c_angry06,
+      c_angry07,
+      c_angry08,
+      c_angry09,
+      c_angry10,
+      c_angry11,
+      c_angry12,
+      c_angry13,
+      c_angry14,
+      c_angry15,
+    ],
   },
   주호민: {
-    joy: [j_joy00, j_joy01],
-    angry: [j_angry00],
+    joy: [j_joy00, j_joy01, j_joy02],
+    angry: [j_angry00, j_angry01, j_angry02, j_angry03, j_angry04],
   },
   주변인물: {
     joy: [o_joy00],
